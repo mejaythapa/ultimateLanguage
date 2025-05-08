5e4f4b4b4be5e7e689d69da0a5d7f82976f3b630
Jay Kumar Thapa <silencemej@gmail.com>
2025-05-08 19:35:28 +1000

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,15 @@
 echo "🚀 Deploying latest changes..."
 
 # Go to app directory
-cd ~/n8n-docker-caddy/iworkmate || exit
+cd ~/n8n-docker-caddy/ultimateLanguage || exit
 
 # Pull latest changes
-git pull origin main
+git pull origin master
 
 # Go back to root folder where docker-compose.yml lives
 cd ..
 
 # Rebuild only web service and restart
-docker compose up -d --build web
+docker compose up -d --build web2
 
 echo "✅ Deployment complete."
